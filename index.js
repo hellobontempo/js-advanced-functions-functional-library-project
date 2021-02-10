59b3dc61abdfa20766ad37a117c3b3b7a01e19cd
claire <clairebontempo@gmail.com>
2021-02-10 15:38:35 -0800

    Done.

--- index.js
+++ index.js
@@ -4,8 +4,16 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
+    each: function(collection, alert) {
+      //iterate over collection, pass each element into function
+      //callback function takes element, index, collection 
+      //if object callback arguments will be value, key, collection
+      const newCollection = (Array.isArray(collection)) ? collection : Object.values(collection)
 
+      for (let i = 0; i < newCollection.length; i++){
+      alert(newCollection[i])}
+      
+      return collection
     },
 
     map: function() {
